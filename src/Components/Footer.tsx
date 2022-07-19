5cc1e3919884f006d24769cc13e8a7848eac4dd2
Gian Manzo <ggianmanzo@gmail.com>
2022-07-19 17:10:24 -0300

    add header / footer / content main

---
+++ src/Components/Footer.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styles from './Footer.module.css'
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <p>
+        <span>Gian Manzo &copy; 2022</span>
+      </p>
+    </footer>
+  )
+}
+
+export default Footer
