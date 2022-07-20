e92b6c965e03fa65ad879b1f11a4a05f9c7bbe6b
Gian Manzo <ggianmanzo@gmail.com>
2022-07-20 10:48:13 -0300

    modified text modal

--- src/Components/Modal.tsx
+++ src/Components/Modal.tsx
@@ -14,7 +14,7 @@ const Modal = ({ children }: Props) => {
     <div id="modal" className="hide">
       <div className={styles.fade} onClick={closeModal}></div>
       <div className={styles.modal}>
-        <h2>Texto modal</h2>
+        <h2>Edit your task </h2>
         {children}
       </div>
     </div>
